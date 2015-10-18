4149b998103d74a99b21314c188ba50d2de22d8d
datrick <heliu@usc.edu>
2015-10-17 21:00:21 -0700

    leetcode 10.17.15

--- Leet199/src/leet199/Solution.java
+++ Leet199/src/leet199/Solution.java
@@ -4,6 +4,23 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Solution {
+
+	private void doRight(TreeNode root, int h, List<Integer> rslt) {
+		if (root == null)
+			return;
+		if (rslt.size() <= h)
+			rslt.add(root.val);
+		else
+			rslt.set(h, root.val);
+		doRight(root.left, h + 1, rslt);
+		doRight(root.right, h + 1, rslt);
+		return;
+	}
+	public List<Integer> rightSideView(TreeNode root) {
+		List<Integer> rslt = new ArrayList<>();
+		doRight(root, 0, rslt);
+		return rslt;
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
