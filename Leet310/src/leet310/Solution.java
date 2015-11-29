7bb2049e1144e00166684aabbf01f72a369343de
datrick <heliu@usc.edu>
2015-11-28 18:03:57 -0800

    database

---
+++ Leet310/src/leet310/Solution.java
@@ -0,0 +1,23 @@
+package leet310;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+
+	public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+		if(n <= 0)
+			return 0;
+		Map<Integer, List<Integer>> tree = new HashMap<>();
+		for (int[] edge: edges) {
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
