01cbffd01e08cda20b7f122c79a524eb8109b751
datrick <heliu@usc.edu>
2016-02-04 10:39:13 -0800

    0204201601

---
+++ Leet331/src/leet331/Solution.java
@@ -0,0 +1,17 @@
+package leet331;
+
+import java.util.Stack;
+
+public class Solution {
+
+	public boolean isValidSerialization(String preorder) {
+		Stack<Character> st = new Stack<>();
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
