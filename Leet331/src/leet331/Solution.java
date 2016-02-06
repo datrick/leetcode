a4bce253bf563d2076c096c87b19b7958b78818a
datrick <heliu@usc.edu>
2016-02-05 19:19:55 -0800

    leet331

--- Leet331/src/leet331/Solution.java
+++ Leet331/src/leet331/Solution.java
@@ -5,10 +5,8 @@ import java.util.Stack;
 public class Solution {
 
 	public boolean isValidSerialization(String preorder) {
-<<<<<<< HEAD
 		Stack<Character> st = new Stack<>();
 		
-=======
 		if (preorder == null || preorder.length() == 0)
 			return true;
 		Stack<String> st = new Stack<>();
@@ -30,17 +28,13 @@ public class Solution {
 //		if (st.isEmpty() && idx == preorder.length())
 //			return true;
 		return false;
->>>>>>> f0b09903c191c7e14c9c1d713867fb13a0e490c4
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-
-=======
 		String preorder = "232,#,#";
 		System.out.println(new Solution().isValidSerialization(preorder));
->>>>>>> f0b09903c191c7e14c9c1d713867fb13a0e490c4
+
 	}
 
 }
